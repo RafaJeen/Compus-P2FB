b387f559ab87262c66a6c3c949e864e5b0e2c650
Rafael Morera Jeeninga <rafael.morera@students.salle.url.edu>
2022-05-09 20:41:44 +0200

    Main structure
    Co-authored-by: mejillones <Edusantosi@users.noreply.github.com>

---
+++ TSIO.c
@@ -0,0 +1,37 @@
+#include "TSIO.h"
+
+
+/*  BSF TXSTA, 5, 0
+    BCF TXSTA, 4, 0
+    BSF TXSTA, 2, 0
+
+    BSF RCSTA, 7, 0
+    BSF RCSTA, 4, 0 ; Enables receiver
+
+    BSF BAUDCON, 3, 0 ; 16 bit baudrate
+
+    ; Baudrate 9600 --> 1040
+    MOVLW HIGH(.1040)
+    MOVWF SPBRGH, 0
+    MOVLW LOW(.1040)
+    MOVWF SPBRG, 0 */
+
+
+
+void initSIO(void) {
+    TXSTAbits.TXEN = 1;
+    TXSTAbits.SYNC = 1;
+    TXSTAbits.BRGH = 1;
+
+    RCSTAbits.SPEN = 1;
+    RCSTAbits.CREN = 1;
+
+    BAUDCONbits.BRG16 = 1;
+
+    //1040 to bin --> 0000 0100 0001 0000
+    SPBRGH = 0b00000100;
+    SPBRG = 0b00010000;
+}
+
+
+
--- main.c
+++ main.c
@@ -1,14 +1,52 @@
 #include <xc.h>
 #include <pic18f4321.h>
+#include "TiTTimer.h"
+#include "LcLCD.h"
+#include "TSIO.h"
 
 
-void initPorts(void) {
+//Configs de asm. El fet de posar pragma ens permetrà accedir al compilador i posar instruccions de asm
+#pragma config OSC = HSPLL
+#pragma config PBADEN = DIG
+#pragma config WDT = OFF
+#pragma config MCLRE = OFF
+#pragma config DEBUG = OFF
+#pragma config PWRT = OFF
+#pragma config BOR = OFF
+#pragma config LVP = OFF
+
+
+
+void __interrupt() RSI_High(void){
+	RSI_Timer0();
+}
+
+void __interrupt(low_priority)  RSI_Low(void) {
 
 }
 
 
+void initPorts(void) {
+    TRISA = 256;
+    TRISB = 0;
+    TRISC = 0b11000000;
+    TRISD = 0;
+}
+
+void initInterrupts(void) {
+ 	RCONbits.IPEN = 0;
+    INTCONbits.GIE = 1;
+}
+
+
+
 void main() {
     initPorts();
+    initInterrupts();
+    LcInit(2, 16);
+    TiInit();
+    initSIO();
+    //initADC();
 
 
     while(1) {
